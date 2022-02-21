1245c81b7d16c7be1ac161dc9c5bfa4e02d9648e
crhistian garcia <garcia.crhistian@correounvalle.edu.co>
2022-02-21 01:54:01 +0100

    NoteScreen body styled

--- 07-journal-app/src/components/notes/NoteScreen.js
+++ 07-journal-app/src/components/notes/NoteScreen.js
@@ -5,6 +5,23 @@ const NoteScreen = () => {
   return (
     <div className='notes__main'>
       <NotesAppBar />
+
+      <div className='notes__content'>
+        <input type='text' className='notes__title--input' placeholder='Entry name' />
+        <textarea
+          name='textarea'
+          className='notes__textarea'
+          placeholder='What did occur me today...'
+        ></textarea>
+
+        <div className='notes__image'>
+          <img
+            src='https://i.ytimg.com/vi/BfCwN4iy6T8/maxresdefault.jpg'
+            alt='purple landscape'
+            height='150px'
+          />
+        </div>
+      </div>
     </div>
   );
 };
