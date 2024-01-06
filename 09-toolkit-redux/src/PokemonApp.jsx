82e6bcc11c1b1ffbb6a095a517e07de0b642cc83
Crhistian Alexander Garcia Urbano <42595236+crhistian2296@users.noreply.github.com>
2024-01-06 20:11:35 +0100

    ADD: pokemoSlice

---
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -0,0 +1,16 @@
+const PokemonApp = () => {
+  return (
+    <>
+      <h1>PokemoApp</h1>
+
+      <hr />
+      <ul>
+        <li>hola </li>
+        <li>hola </li>
+        <li>hola </li>
+      </ul>
+    </>
+  );
+};
+
+export default PokemonApp;
