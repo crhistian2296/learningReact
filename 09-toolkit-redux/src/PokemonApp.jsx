880443d7341b2ffa83ef9e8e58e23b0e7f088fc7
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2024-01-08 19:37:57 +0100

    ADD: PokeApp with pagination

--- 09-toolkit-redux/src/PokemonApp.jsx
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getPokemons } from './store/slices/pokemon';
-import { useCallback } from 'react';
 
 const PokemonApp = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const dispatch = useCallback(useDispatch(), []);
+  const { pokemons, isLoading, page } = useSelector((state) => state.pokemons);
 
   useEffect(() => {
     dispatch(getPokemons());
@@ -16,11 +16,35 @@ const PokemonApp = () => {
       <h1>PokemoApp</h1>
 
       <hr />
-      <ul>
-        <li>hola </li>
-        <li>hola </li>
-        <li>hola </li>
+      <p>Loading: {String(isLoading)} </p>
+      <h3>Page: {page}</h3>
+      <ul style={{ listStyle: 'none' }}>
+        {pokemons &&
+          pokemons.map((pokemon, index) => {
+            let id = pokemon.url.split('/').at(-2);
+            return (
+              <li key={index}>
+                {id}. {pokemon.name}
+              </li>
+            );
+          })}
       </ul>
+
+      <button
+        disabled={page === 1 && true}
+        onClick={() => {
+          dispatch(getPokemons(page - 2));
+        }}
+      >
+        PREV
+      </button>
+      <button
+        onClick={() => {
+          dispatch(getPokemons(page));
+        }}
+      >
+        NEXT
+      </button>
     </>
   );
 };
