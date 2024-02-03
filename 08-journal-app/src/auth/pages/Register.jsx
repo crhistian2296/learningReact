5b625d76eb29a2793522a442a2265d6f64fc69eb
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2024-02-03 21:22:06 +0100

    ADD: Validations controled

--- 08-journal-app/src/auth/pages/Register.jsx
+++ 08-journal-app/src/auth/pages/Register.jsx
@@ -1,35 +1,82 @@
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Link as RouteLink } from 'react-router-dom';
+import { useForm } from '../../hooks';
 import AuthLayout from '../layout/AuthLayout';
 
+const formData = {
+  displayName: 'crhistian',
+  email: 'user@mail.com',
+  password: 1234,
+};
+
+const formValidations = {
+  email: [value => value?.includes('@'), 'The email must have @'],
+  password: [
+    value => value?.length >= 6,
+    'The password must have more than 6 charactes',
+  ],
+  displayName: [value => value?.length >= 1, 'The name is needed'],
+};
+
 const Register = () => {
+  const {
+    displayName,
+    email,
+    password,
+    onInputChange,
+    formState,
+    displayNameValid,
+    emailValid,
+    passwordValid,
+  } = useForm(formData, formValidations);
+
+  console.log(displayNameValid, emailValid, passwordValid);
+
+  const onSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
     <AuthLayout title='Register'>
-      <form>
+      <form onSubmit={e => onSubmit(e)}>
         <Grid container>
           <Grid item xs={12} mt={2}>
-            <TextField label='name' type='name' fullWidth />
+            <TextField
+              label='displayName'
+              type='displayName'
+              name='displayName'
+              fullWidth
+              autoComplete='displayName'
+              value={displayName}
+              onChange={onInputChange}
+            />
           </Grid>
           <Grid item xs={12} mt={2}>
             <TextField
               label='email'
               type='email'
+              name='email'
               fullWidth
               autoComplete='email'
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} mt={2}>
             <TextField
               label='password'
               type='password'
+              name='password'
               fullWidth
               autoComplete='current-password'
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
         </Grid>
         <Grid container spacing={2} mt={1}>
           <Grid item xs={12} sm={6}>
-            <Button variant='contained' fullWidth>
+            <Button variant='contained' fullWidth type='submit'>
               Create account
             </Button>
           </Grid>
--- 08-journal-app/src/hooks/useForm.js
+++ 08-journal-app/src/hooks/useForm.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-export const useForm = (initialForm = {}) => {
+export const useForm = (initialForm = {}, formValidations = {}) => {
   const [formState, setFormState] = useState(initialForm);
+  const [formValidation, setFormValidation] = useState({});
 
   const onInputChange = ({ target }) => {
     const { name, value } = target;
@@ -15,10 +16,31 @@ export const useForm = (initialForm = {}) => {
     setFormState(initialForm);
   };
 
+  useEffect(() => {
+    createValidators();
+  }, [formState]);
+
+  const createValidators = () => {
+    const formCheckedValues = {};
+
+    for (const formField of Object.keys(formValidations)) {
+      const [fn, errorMessage] = formValidations[formField];
+
+      formCheckedValues[`${formField}Valid`] = fn(formState[formField])
+        ? null
+        : errorMessage;
+    }
+
+    setFormValidation(formCheckedValues);
+  };
+
+  console.log(formState.displayName);
+
   return {
     ...formState,
     formState,
     onInputChange,
     onResetForm,
+    ...formValidation,
   };
 };
