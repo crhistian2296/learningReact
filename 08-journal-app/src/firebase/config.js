a8364271eb2a7fdcd36797d0c82fd2fdcfa3c7ea
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2024-03-05 23:41:59 +0100

    ADD: enviroment variables

--- 08-journal-app/src/firebase/config.js
+++ 08-journal-app/src/firebase/config.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,16 +7,15 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyACUE6OwEPxDTm7FPaWKWzB4KxXkmda0WE",
-  authDomain: "journal-d1261.firebaseapp.com",
-  projectId: "journal-d1261",
-  storageBucket: "journal-d1261.appspot.com",
-  messagingSenderId: "741085925823",
-  appId: "1:741085925823:web:5e7aac120c6cfbb984110a"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
 export const FirebaseAuth = getAuth(FirebaseApp);
 export const FirebasDB = getFirestore(FirebaseApp);
-
